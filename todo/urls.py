4dd4189d20d1c9444d1476e8256e2740d4e52ee6
user <user@usernoMacBook-Air.local>
2024-07-23 16:14:29 +0900

    commit3

--- todo/urls.py
+++ todo/urls.py
@@ -7,4 +7,5 @@ app_name='todo'
 
 urlpatterns = [
     path('',views.IndexView.as_view(),name='index'),
+    path('create/',views.CreateView.as_view(),name='create'),
 ]
--- todo/views.py
+++ todo/views.py
@@ -1,8 +1,14 @@
 from django.shortcuts import render
 from .models import Post
 from django.views import generic
+from .forms import PostCreateForm
 
 class IndexView(generic.ListView):
     model=Post
     
+class CreateView(generic.CreateView): 
+    model=Post
+    form_class=PostCreateForm
     
+    def __str__(self):
+        return self.title
