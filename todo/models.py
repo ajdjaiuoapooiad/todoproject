23d4ede75ac65e548d266c9c90bfccc5f8415f16
user <user@usernoMacBook-Air.local>
2024-07-23 18:03:23 +0900

    create

--- todo/models.py
+++ todo/models.py
@@ -1,9 +1,17 @@
 from django.db import models
 
+PRIORITY=(('danger','high'),('info','normal'),('success','low'))
 
 class Post(models.Model):
     title=models.CharField('タイトル',max_length=30)
     text=models.TextField('本文')
+    priority=models.CharField(
+        '優先度',
+        max_length=50,
+        choices=PRIORITY,
+        null=True
+    )
+    duedate=models.DateField('期限',null=True)
     
     def __str__(self):
         return self.title
--- todo/views.py
+++ todo/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.urls import reverse_lazy
 from .models import Post
 from django.views import generic
 from .forms import PostCreateForm
@@ -9,6 +9,7 @@ class IndexView(generic.ListView):
 class CreateView(generic.CreateView): 
     model=Post
     form_class=PostCreateForm
+    success_url=reverse_lazy('todo:index')
     
     def __str__(self):
         return self.title
