7ac936b9226c3002a307b222585ba1a064e7032e
user <user@usernoMacBook-Air.local>
2024-07-24 12:59:10 +0900

    delete

--- todo/urls.py
+++ todo/urls.py
@@ -8,4 +8,5 @@ app_name='todo'
 urlpatterns = [
     path('',views.IndexView.as_view(),name='index'),
     path('create/',views.CreateView.as_view(),name='create'),
+    path('delete/<int:pk>',views.DeleteView.as_view(),name='delete'),
 ]
--- todo/views.py
+++ todo/views.py
@@ -13,3 +13,7 @@ class CreateView(generic.CreateView):
     
     def __str__(self):
         return self.title
+    
+class DeleteView(generic.DeleteView):
+    model=Post
+    success_url=reverse_lazy('todo:index')
